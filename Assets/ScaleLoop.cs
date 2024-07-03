f042fbebc1feebe66b433140b07b7c1cd9e6fde9
yanaidul <71026280+yanaidul@users.noreply.github.com>
2024-07-03 17:16:53 +0700

    Commit from kantor 03072024

---
+++ Assets/ScaleLoop.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ScaleLoop : MonoBehaviour
+{
+    [SerializeField] private Image imageToScale;
+    private float scaleDuration = 1f;
+    private float minScale = 0.8f;
+    private float maxScale = 1.2f; 
+
+    void Start()
+    {
+        if (imageToScale != null)
+        {
+            // Scale up and down in a loop
+            imageToScale.transform.DOScale(maxScale, scaleDuration)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.InOutSine);
+        }
+    }
+}
---
+++ Assets/ScannerDetector.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScannerDetector : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _onActionUltraSonicScalerSpriteRenderer;
+    [SerializeField] private BoxCollider2D _onActionUltraSonicScalerBoxCollider;
+
+    private Vector2 _initPos;
+
+    private void Start()
+    {
+        _onActionUltraSonicScalerSpriteRenderer.gameObject.SetActive(false);
+        _onActionUltraSonicScalerBoxCollider.enabled = true;
+        _initPos = transform.localPosition;
+    }
+
+    //Code yang digunakan untuk mendeteksi gameobject yang memiliki tag "KarangGigi", dan melaksanakan animasi motion yang singkan lalu menghilangkan karang gigi
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GigiBerlubang"))
+        {
+            gameObject.transform.position = collision.transform.position;
+            _onActionUltraSonicScalerBoxCollider.enabled = false;
+            _onActionUltraSonicScalerSpriteRenderer.gameObject.SetActive(true);
+            transform.DOMoveX(transform.position.x - 0.5f, 0.3f).SetLoops(3, LoopType.Yoyo).OnComplete(() =>
+            {
+                KarangGigiGameManager.GetInstance().RemoveKarangGigiFromList(collision.gameObject);
+                collision.gameObject.SetActive(false);
+                transform.localPosition = _initPos;
+                _onActionUltraSonicScalerSpriteRenderer.gameObject.SetActive(false);
+                _onActionUltraSonicScalerBoxCollider.enabled = true;
+            });
+
+        }
+    }
+}
